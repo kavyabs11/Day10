ed233f5e6a0ff95a7b2de3b8ad6add59b36ba3e0
kavyabs11 <kavyasomshekar11@gmail.com>
2022-09-15 03:30:49 -0700

    EmpWage builder using interface

---
+++ EmpWage/EmmployeeWageForCompany.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage
+{
+    public interface IEmployeeWageForCompany
+    {
+        public void AddDetails(String company, int WaPhr, int max_workDays, int max_workHrs);
+        public void perCompany();
+        public int wage(int wage);
+        public int compute(ParticularCompany company);
+    }
+}
